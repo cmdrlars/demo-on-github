7e60ef061cfb142ae30c8d4f62133e9195cae5d4
lhenning <lars.hennings@bonprix.net>
2022-03-30 13:40:25 +0200

    first commit

---
+++ src/main/java/com/example/demoongithub/DemoOnGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoongithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOnGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOnGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoongithub/DemoOnGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoongithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOnGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
